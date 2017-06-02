b2987f7bb5f46834e461db87f4ced8d5190fd996
adolf <adolf@123-204-135-167.adsl.dynamic.seed.net.tw>
2017-06-02 23:31:30 +0800

    download program with ffmpeg

--- pts_vod7.py
+++ pts_vod7.py
@@ -2,23 +2,37 @@
 from bs4 import BeautifulSoup
 import urllib3
 import urllib.parse
+import subprocess
 
-http = urllib3.PoolManager()
-r = http.request('GET', 'http://vod7.pts.org.tw/cate/4#cbpf=.item25')
-soup = BeautifulSoup(r.data, "html.parser")
 
-divs = soup.select("div.cbp-l-grid-agency.cbp")
-titles = soup.select("div.cbp-l-grid-agency-title")
-for anchor in soup.findAll('a', class_="cbp-lightbox", href=True):
-    u = anchor['href']
-    print(urllib.parse.unquote(u))
+def listPrograms(pageUrl):
+    http = urllib3.PoolManager()
+    r = http.request('GET', pageUrl)
+    soup = BeautifulSoup(r.data, "html.parser")
+    programs = []
+    for anchor in soup.findAll(True, {'class': ['cbp-item']}):
+        v = anchor.find_all('li')
+        title = anchor.find_all(True, {'class': ['cbp-l-grid-agency-title']})
+        # print(title[0].text)
+        long_desc = anchor.find_all(True, {'class': ['cbp-l-grid-agency-desc']})
+        # print(long_desc[0].text)
+        href = v[0].findAll('a', class_="cbp-lightbox", href=True)[0]['href']
+        # print(urllib.parse.unquote(href))
+        program = {'title': title[0].text, 'desc': long_desc[0].text, 'url': urllib.parse.unquote(href)}
+        programs.append(program)
+    return programs
 
-found = soup.findAll(True, {'class': ['cbp-item']})
-for anchor in soup.findAll(True, {'class': ['cbp-item']}):
-    v = anchor.find_all('li')
-    title = anchor.find_all(True, {'class': ['cbp-l-grid-agency-title']})
-    print(title[0].text)
-    long_desc = anchor.find_all(True, {'class':['cbp-l-grid-agency-desc']})
-    print(long_desc[0].text)
-    href = v[0].findAll('a', class_="cbp-lightbox", href=True)[0]['href']
-    print(urllib.parse.unquote(href))
+
+def selectPrograms(programs):
+    for i in range(programs.__len__()):
+        print("%d: %s\n%s\n" % (i, programs[i]['title'], programs[i]['desc']))
+    nb = input('numbers: ')
+    nums = [int(s) for s in nb.split() if s.isdigit()]
+    for j in nums:
+        subprocess.call(['ffmpeg', '-i', programs[j]['url'], '-codec', 'copy', programs[j]['title'] + ".mp4"])
+
+
+programs = listPrograms('http://vod7.pts.org.tw/cate/4#cbpf=.item25')
+selectPrograms(programs)
+programs = listPrograms('http://vod7.pts.org.tw/cate/5#cbpf=.item29')
+selectPrograms(programs)
